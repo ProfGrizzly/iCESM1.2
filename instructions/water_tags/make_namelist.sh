b197f47044101e7a6a164ad1281ea6e06429d57d
Theodor Mayer <theodor.mayer@uconn.edu>
2021-12-14 07:35:03 -0700

    make_tag script

---
+++ instructions/water_tags/make_namelist.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+## Author: Theodor Mayer
+## To report bug, please contact theodorm@uconn.edu
+## This script uses the location tags and isotope tages to set up namelist for water tagging function in CAM5
+## It can be used as a standalone tool,
+## or part of the make_tag.csh to set up the namelist after constructing the tagging code
+###### for standalone execution
+#loctags=( LND OCN NP WEP EEP WNP ENP WSP ESP SO TIN EAM AM )
+#topetags=(_W _16 _D _18) # water, h216O, hDO or h218O (corresponding to the tagged species)
+#############################
+#set up wtrc_names, > overwrite, >> add
+echo "making namelist file for tagged regions and water species"
+echo ${loctags[*]} 
+echo ${topetags[*]}
+species=(H2O H216O HD16O H218O)
+wtrc_tails=(V L I R S r s)
+pcp_tails=(R S r s)
+
+if [[ ! -f user_nl_cam ]] ; then
+	touch user_nl_cam
+fi
+###################################
+cat << EOF >> user_nl_cam 
+wtrc_names             = 'H2OV', 'H2OL', 'H2OI', 'H2OR', 'H2OS', 'H2Or', 'H2Os',
+                          'H216OV', 'H216OL', 'H216OI','H216OR','H216OS','H216Or', 'H216Os',
+                          'HDOV','HDOL', 'HDOI', 'HDOR', 'HDOS', 'HDOr', 'HDOs',
+                          'H218OV', 'H218OL', 'H218OI', 'H218OR', 'H218OS', 'H218Or', 'H218Os',
+EOF
+#########loop through to set the wtrc_names
+for topetag in ${topetags[*]}
+do
+for loctag in ${loctags[*]} 
+do
+cat << EOF >> user_nl_cam
+	                  '$loctag$topetag${wtrc_tails[0]}','$loctag$topetag${wtrc_tails[1]}', '$loctag$topetag${wtrc_tails[2]}', '$loctag$topetag${wtrc_tails[3]}', '$loctag$topetag${wtrc_tails[4]}', '$loctag$topetag${wtrc_tails[5]}', '$loctag$topetag${wtrc_tails[6]}',
+EOF
+done
+done
+truncate -s-2 user_nl_cam
+echo "" >> user_nl_cam
+### done of loop
+##############set up wtrc_srfpcp_names
+cat << EOF >> user_nl_cam
+wtrc_srfpcp_names          =  'H2OR', 'H2OS', 'H2Or', 'H2Os',
+                              'H216OR', 'H216OS', 'H216Or', 'H216Os',
+                              'HDOR', 'HDOS', 'HDOr', 'HDOs',
+                              'H218OR', 'H218OS', 'H218Or', 'H218Os',
+EOF
+for topetag in ${topetags[@]}
+do
+for loctag in ${loctags[@]}
+do
+cat << EOF >> user_nl_cam
+                              '$loctag$topetag${pcp_tails[0]}','$loctag$topetag${pcp_tails[1]}', '$loctag$topetag${pcp_tails[2]}', '$loctag$topetag${pcp_tails[3]}',
+EOF
+done
+done
+truncate -s-2 user_nl_cam
+echo "" >> user_nl_cam
+#############set up wtrc_species_names
+cat << EOF >> user_nl_cam
+wtrc_species_names         = 'H2O', 'H2O', 'H2O', 'H2O', 'H2O', 'H2O', 'H2O',
+                             'H216O', 'H216O', 'H216O', 'H216O', 'H216O', 'H216O', 'H216O',
+                             'HD16O', 'HD16O', 'HD16O', 'HD16O', 'HD16O', 'HD16O', 'HD16O',
+                             'H218O', 'H218O', 'H218O', 'H218O', 'H218O', 'H218O', 'H218O',
+EOF
+for i in ${!topetags[@]} ##give us array indexes
+do
+for j in ${loctags[*]}
+do
+cat << EOF >> user_nl_cam
+	                     '${species[i]}', '${species[i]}', '${species[i]}', '${species[i]}', '${species[i]}', '${species[i]}', '${species[i]}',
+EOF
+done 
+done
+truncate -s-2 user_nl_cam
+echo "" >> user_nl_cam
+############set up the  wtrc_srfvap_names              = 'H2OV', 'H216OV', 'HDOV','H218OV',
+cat << EOF >> user_nl_cam
+wtrc_srfvap_names              = 'H2OV', 'H216OV', 'HDOV','H218OV',
+EOF
+for topetag in ${topetags[*]}
+do
+for loctag in ${loctags[*]}
+do
+cat << EOF >> user_nl_cam
+			         '${loctag}${topetag}V',
+EOF
+done 
+done
+truncate -s-2 user_nl_cam
+echo "" >> user_nl_cam
+########set up wtrc_type_names
+cat << EOF >> user_nl_cam
+wtrc_type_names           =  
+EOF
+truncate -s-1 user_nl_cam
+c=$((${#topetags[*]}*${#loctags[*]}))+4
+for ((i=0; i<$c; i++))
+do
+cat << EOF >> user_nl_cam
+'VAPOR', 'LIQUID', 'ICE', 'RAINS', 'SNOWS', 'RAINC', 'SNOWC',
+EOF
+done
+truncate -s-2 user_nl_cam
+echo "" >> user_nl_cam
---
+++ instructions/water_tags/make_tag.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+#author: Theodor Mayer
+#contact: 
+################################################
+# user defined tagging regions and tracer species
+###############################################
+tracking=( 0 1 1 1 ) #which iso (isph2o isph216o isphdo isph218o) to track, include = 1, dont include = 0
+####################################
+#isotope tag for the output history file - inputs for make_namelist.sh to set up user_nl_cam
+export topetags=(_16 _D _18) #corresponding to the tagged water species
+####################################
+#region tag from water tagging: for both make_tag.sh and make_namelist.sh
+export loctags=( LND OCN NP WEP EEP WNP ENP WSP ESP SO TIN EAM AM )
+####################################
+# whether to tag evaporation from ocean, land or ice
+ifocn=( 0 1 1 1 1 1 1 1 1 1 1 0 0 ) 
+iflnd=( 1 0 0 0 0 0 0 0 0 0 0 1 1 ) 
+ifice=( 0 0 0 0 0 0 0 0 0 0 0 0 0 )
+##################################### 
+### latitude, longitude bound:needs to be integers
+##       latupp 
+#  lonlow      lonupp
+##      latlow
+latlow=( -90 -90   30 -10 -10 -10 10  -30 -30 -90 -10  10 -20) #lat lower bound 
+latupp=(  90  90   60  30  10  30 30  -10 -10 -30  30  30 -10)  #lat upper bound
+lonlow=(  0   0   120  90 150 150 210 150 210   0  40  70 110) #lon lower bound
+lonupp=(  360 360 250 150 275 210 275 210 275 360  90 130 160) #lon upper bound
+#########
+#tag for the output history file - for make_namelist.sh
+export topetags=(_16 _D _18) #corresponding to the tagged water species
+## the naming convention for the output variables is {loctags}{topetags}
+#############################
+#internal definitions of the code : no need to change, unless the code base is changed
+#############################
+###case name of different water species
+casename=( isph2o isph216o isphdo isph218o )
+#############################
+# the coded tags for different species in evaporation and condensation
+#############################
+evaptags=( "" _16O _HDO _18O )
+jind=5  # indices for counting tracers, starting with 5, first four are H2O, H216O, H218O and HDO
+#############################
+# end of internal definition
+############################
+## record the section of code for water tagging as a temporary file
+rm -rf tag_section.temp
+touch  tag_section.temp 
+###########################
+if [[ ${#tracking[*]} > 4 ]] ; then
+	echo "tracking option can only have four elements"	
+fi
+for i in ${!tracking[@]}; do
+        if [[ ${tracking[i]} == 0 ]] ;then
+####  not tracking ${casename[i]} 
+echo "
+              case (${casename[i]})
+                cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = -x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) " >> tag_section.temp
+#### "tracking ${casename[i]}"
+        elif [[ ${tracking[i]} == 1 ]] ;then
+### i+1 is the fortran index, which starts at 1
+echo "
+              case (${casename[i]})
+                if(j .eq. $(($i + 1))) then 
+                  cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = -x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig)
+                else !water tag
+                  if( -x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) .lt. 0._r8) then !dew/frost?
+                    !calculate surface vapor ratio:
+                     R = wtrc_ratio(wtrc_species(wtrc_iasrfvap(j)),cam_out(c)%qbot(i,wtrc_indices(wtrc_iasrfvap(j))),&
+                                    cam_out(c)%qbot(i,wtrc_indices(wtrc_iasrfvap(1))))
+                      cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = R*-x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) " >> tag_section.temp
+echo "	        else " 														>> tag_section.temp
+		for j in ${!loctags[@]}; do
+echo " 		     if(j .eq. ${jind}) then "											>> tag_section.temp
+echo "  	             if(((wtlat > ${latlow[j]}._r8) .and. (wtlat < ${latupp[j]}._r8)) .and. ((wtlon > ${lonlow[j]}._r8) .and. (wtlon <= ${lonupp[j]}._r8))) then " >> tag_section.temp
+		if [[ ${ifocn[j]} == 1 ]]; then
+echo "		     			cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = (1._r8-cam_in(c)%landfrac(i))*-x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) " >> tag_section.temp
+		elif [[ ${ifice[j]} == 1 ]]; then
+echo "     				cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = (1._r8-cam_in(c)%landfrac(i)-cam_in(c)%ocnfrac(i))*-x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) " >> tag_section.temp
+		elif [[ ${iflnd[j]} == 1 ]]; then
+echo "     				cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = cam_in(c)%landfrac(i)*-x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) " >> tag_section.temp
+		else	
+echo "     				cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = -x2a_a%rAttr(index_x2a_Faxx_evap${evaptags[i]},ig) " >> tag_section.temp
+		fi
+echo "   	              else
+					cam_in(c)%cflx(i,wtrc_indices(wtrc_iasrfvap(j))) = 0._r8
+		              end if 
+		     end if " >> tag_section.temp
+###############only add the last part to the end of the if statements within the loop
+                if [[ $(($j+1)) == ${#loctags[@]} ]]; then
+echo "
+                  end if   !dew/frost
+                end if     !H2O tracer " >> tag_section.temp
+                fi
+###############advance the counter for counting tracers
+		     jind=$(($jind + 1))
+		done 
+	else
+		echo "tracking value must be 0 or 1"
+	fi
+done
+################# end of constructing the water tagging code, temporary output is tag_section.temp
+### check if atm_comp_mct.F90 already exists in the current folder
+nlines=`wc -l < tag_section.temp`
+rlines=`awk '/water\ tag\ goes\ here/{print NR}' atm_comp_mct_template_F90`
+
+if [[ -f atm_comp_mct.F90 ]]; then
+   read -p "found existing atm_comp_mct.F90. wish to delete? (yes or no)" yn
+   case $yn in
+        [Yy]* ) rm atm_comp_mct.F90 ; 
+	### insert the section of code with water tagging
+		sed ''${rlines}' e sed -n 1,'${nlines}'p tag_section.temp' atm_comp_mct_template_F90 > atm_comp_mct.F90 ;
+		rm tag_section.temp ;;
+        [Nn]* ) echo "nothing is done to atm_comp_mct.F90; partial code is saved as tag_section.temp" ;;
+   esac
+else
+		sed ''${rlines}' e sed -n 1,'${nlines}'p tag_section.temp' atm_comp_mct_template_F90 > atm_comp_mct.F90
+		rm tag_section.temp
+fi
+################
+### check if user_nl_cam already exists in the current folder; and whether the user wants to append a new section
+### of water tagging setup
+if [[ -f user_nl_cam ]]; then
+   read -p "found existing user_nl_cam. Wish to append namelist settings for water tagging to the existing file? (yes or no)" yn
+   case $yn in
+        [Yy]* ) source ./make_namelist.sh ;;
+        [Nn]* ) echo "nothing is done to user_nl_cam" ;;
+   esac
+else
+	source ./make_namelist.sh
+fi
+###run 
